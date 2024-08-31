7ab742279d13225ed2b033047db6620cbfa9f478
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-08-31 11:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ def main():
 
     st.write("Click the links below to redirect to the components of the FinAgent:")
 
-    st.markdown("[FinAgent360 - Individual Advisory (LLM+FinBERT Powered)]()")
+    st.markdown("[FinAgent360 - Individual Advisory (LLM+FinBERT Powered)](https://your-url-here)")
     st.markdown("[FinAgent Future Stock Prediction Model](https://smart-healthcare-assistant-prasunethon.streamlit.app/)")
     st.markdown("[FinAgent Insight - Validation Based Stocks Education Content](https://smart-city-management.streamlit.app/)")
 
-if name == "main":
+if __name__ == "__main__":
     main()
