aebef8587d92356e73c31d636e8c382d9cdc2112
Pratap <thepratappawar@gmail.com>
2024-08-31 11:42:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.title("Team FinAgent Components - RAKATHON2024")
+
+    st.write("Click the links below to redirect to the components of the FinAgent:")
+
+    st.markdown("[FinAgent360 - Individual Advisory (LLM+FinBERT Powered)]()")
+    st.markdown("[FinAgent Future Stock Prediction Model](https://smart-healthcare-assistant-prasunethon.streamlit.app/)")
+    st.markdown("[FinAgent Insight - Validation Based Stocks Education Content](https://smart-city-management.streamlit.app/)")
+
+if name == "main":
+    main()
